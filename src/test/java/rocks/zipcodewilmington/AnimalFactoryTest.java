ccb6feb361eb915daf2afd653254aa016b977027
cwilliams <coreywllms9@gmail.com>
2020-02-28 18:04:20 -0500

    completed create cat test

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -2,6 +2,7 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 
@@ -22,7 +23,15 @@ public class AnimalFactoryTest {
         Dog referenceTheDogThatIJustCreated = factory.createDog(referenceTheNameIGaveThisDog,referenceTheDateIGaveThisDog);
         Assert.assertTrue(referenceTheDogThatIJustCreated instanceof Dog);
     }
+ @Test
+    public void testCreatCat(){
+     AnimalFactory factory = new AnimalFactory();
+     String referenceTheNameIGaveThisDog = "Pam";
+     Date referenceTheDateIGaveThisDog = new Date(2020);
+     Cat referenceTheCatThatIJustCreated = factory.createCat(referenceTheNameIGaveThisDog,referenceTheDateIGaveThisDog);
+     Assert.assertTrue(referenceTheCatThatIJustCreated instanceof Cat);
 
+        }
 
 
 }
