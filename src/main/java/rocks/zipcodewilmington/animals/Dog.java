4e4cf13c63734e68b54d388d40111772fb99b0d9
cwilliams <coreywllms9@gmail.com>
2020-02-28 17:58:21 -0500

    animal factory still in progress on cat test

--- src/main/java/rocks/zipcodewilmington/animals/Dog.java
+++ src/main/java/rocks/zipcodewilmington/animals/Dog.java
@@ -6,11 +6,26 @@ import java.util.Date;
  * @author leon on 4/19/18.
  */
 public class Dog extends Mammal {
+
     public Dog(String name, Date birthDate, Integer id) {
         super(name, birthDate, id);
     }
+    //Dog(){} <-Constructor
+
+    //speak(){} <-Method
+
+    //anything(){} <-Method
+
+    //anything(String whatImCallingIt) <-Method with parameters
+
+    //String steelers = "Big Ben"; <-M
+
+    //anything(steelers)
+
+
 
     public String speak() {
         return "bark!";
     }
+
 }
--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,33 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+    public void testCreatDog(){
+        AnimalFactory factory = new AnimalFactory();
+        String referenceTheNameIGaveThisDog = "Bob";
+        Date referenceTheDateIGaveThisDog = new Date(2020);
+        Dog referenceTheDogThatIJustCreated = factory.createDog(referenceTheNameIGaveThisDog,referenceTheDateIGaveThisDog);
+        Assert.assertTrue(referenceTheDogThatIJustCreated instanceof Dog);
+    }
+
+
+
 }
+
+
+
+
+
