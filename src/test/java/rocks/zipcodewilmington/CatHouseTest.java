3e5a9acc79caab51284a4eb232611d00324f580b
cwilliams <coreywllms9@gmail.com>
2020-02-29 09:15:36 -0500

    completed add and remove on cat house

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,5 +1,10 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+
 /**
  * @author leon on 4/19/18.
  */
@@ -9,4 +14,29 @@ public class CatHouseTest {
     // TODO - Create tests for `void remove(Cat cat)`
     // TODO - Create tests for `Cat getCatById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfCats()`
+
+    @Test
+    public void addTest(){
+
+        Cat testCat = new Cat("tom",null, 25);
+        CatHouse testCatHouse = new CatHouse();
+        testCatHouse.add(testCat);
+        int result = CatHouse.getNumberOfCats();
+        Assert.assertEquals(1,result);
+    }
+    @Test
+    public void removeTest(){
+        Cat testCat = new Cat("tom",null, 25);
+        CatHouse testCatHouse = new CatHouse();
+        testCatHouse.remove(testCat);
+        int result = CatHouse.getNumberOfCats();
+        Assert.assertEquals(0,result);
+    }
+
+
+
+
+
+
+
 }
